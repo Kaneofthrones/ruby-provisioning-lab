60ecab12671831c86e0af841397fbdf6321fd11f
Kane Marcus <kaneofthrones@gmail.com>
2017-09-12 10:20:25 +0100

    0 failures

--- environment/provision.sh
+++ environment/provision.sh
@@ -4,3 +4,10 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-get update
+sudo apt-get install build-essential libgmp-dev libxml2 -y
+sudo apt-get install ruby2.4 ruby2.4-dev ruby-build -y
+
+sudo gem install bundler
+
